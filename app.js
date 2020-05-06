84a31e03a049fd57b460de79ebee266092cdbd72
Gediminas Kailiūnas <43819463+Paranite@users.noreply.github.com>
2020-05-06 22:28:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -42,6 +42,3 @@ httpsLocalhost.getCerts().then(certs => {
         console.log("Server running at https://localhost:" + port)
     })
 })
-// app.listen(3000, function() {
-//   console.log("Server started on port 3000");
-// });
